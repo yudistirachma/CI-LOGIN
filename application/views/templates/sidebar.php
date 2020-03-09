70e192fe25b1b00b8446ef69d572bc1230a79bde
rahma yudistira <yudistira.anaga@gmail.com>
2020-03-09 21:36:12 +0700

    finis vidio #6 adding fiture dinamic sidebar for specific role

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,33 +12,44 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Administrator
-    </div>
+    <!-- query menu -->
+    <?php
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT `user_menu`.`id`, `menu` 
+    FROM user_menu JOIN user_access_menu
+    ON user_menu.id = user_access_menu.menu_id
+    WHERE user_access_menu.role_id = $role_id
+    ORDER BY user_access_menu.menu_id ASC
+    ";
+    $menu = $this->db->query($queryMenu)->result_array();
+    ?>
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
-
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-user-alt"></i>
-            <span>My Profile</span></a>
-    </li>
-
-    <hr class="sidebar-divider">
+    <!-- looping menu -->
+    <?php foreach ($menu as $m) : ?>
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            <?= $m['menu']; ?>
+        </div>
+        <!-- prepare sub menu -->
+        <?php
+        $querySubMenu = "SELECT * 
+        FROM user_sub_menu
+        WHERE menu_id = {$m['id']}
+        AND is_active = 1
+        ";
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
+        <?php foreach ($subMenu as $sm) : ?>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url($sm['url']) ?>">
+                    <i class="<?= $sm['icon'] ?>"></i>
+                    <span><?php echo ucwords($sm['title']); ?></span></a>
+            </li>
+        <?php endforeach; ?>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+    <?php endforeach; ?>
 
     <li class="nav-item">
         <a class="nav-link" href="<?= base_url('auth/logout') ?>">
